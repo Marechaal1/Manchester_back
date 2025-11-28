5422822be47ec22919a59aeb2120227f7d3f86da
Marechaal1 <alexandrex146@gmail.com>
2025-11-27 22:00:23 -0300

    Upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,4 @@
-
-set -e
-
-
-#!/usr/bin/env sh
+#!/bin/sh
 set -e
 
 mkdir -p storage/framework/cache \
@@ -10,18 +6,8 @@ mkdir -p storage/framework/cache \
     storage/framework/views \
     bootstrap/cache
 
-
 chown -R www-data:www-data storage bootstrap/cache || true
 chmod -R ug+rwX storage bootstrap/cache || true
-
-if [ -f "composer.json" ] && [ ! -d "vendor" ]; then
-	composer install --no-dev --no-interaction --optimize-autoloader || true
-fi
-
-exec "$@"
-
-set -e
-
 
 if [ -f "composer.json" ] && [ ! -d "vendor" ]; then
 	composer install --no-dev --no-interaction --optimize-autoloader || true
